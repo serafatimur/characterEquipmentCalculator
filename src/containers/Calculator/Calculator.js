55d9a8cc5e602e84df02c5dff618ccb76a570969
serafatimur <serafatimur@gmail.com>
2019-08-22 15:51:36 +0300

    load data from json file

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -5,157 +5,8 @@ import Shop from '../../components/Shop/Shop'
 
 class Calculator extends Component {
     state = {
-        "person": {
-            "id": "0",
-            "name": "Hero",
-            "armor": "10",
-            "attack": "25",
-            "health": "100",
-            "img": "http://test.datalb.ru/assets/Dude-1.png",
-            "alt-img": "http://test.datalb.ru/assets/Dude-2.png"
-        },
-        "equipment": {
-            "helmets": 
-            [{
-                "id": "0",
-                "name": "gray helmet",
-                "price": "100",
-                "armor": "2",
-                "attack": "0",
-                "health": "5",
-                "img": "http://test.datalb.ru/assets/Helmet-1.png"
-            },
-            {
-                "id": "1",
-                "name": "green helmet",
-                "price": "200",
-                "armor": "4",
-                "attack": "0",
-                "health": "10",
-                "img": "http://test.datalb.ru/assets/Helmet-2.png"
-            },
-            {
-                "id": "2",
-                "name": "purple helmet",
-                "price": "400",
-                "armor": "6",
-                "attack": "0",
-                "health": "15",
-                "img": "http://test.datalb.ru/assets/Helmet-3.png"
-            }],
-            "gloves": 
-            [{
-                "id": "0",
-                "name": "gray gloves",
-                "price": "100",
-                "armor": "2",
-                "attack": "0",
-                "health": "5",
-                "img": "http://test.datalb.ru/assets/Gloves-1.png"
-            },
-            {
-                "id": "1",
-                "name": "green gloves",
-                "price": "200",
-                "armor": "4",
-                "attack": "0",
-                "health": "10",
-                "img": "http://test.datalb.ru/assets/Gloves-2.png"
-            },
-            {
-                "id": "2",
-                "name": "purple gloves",
-                "price": "400",
-                "armor": "6",
-                "attack": "0",
-                "health": "15",
-                "img": "http://test.datalb.ru/assets/Gloves-3.png"
-            }],
-            "chests": 
-            [{
-                "id": "0",
-                "name": "gray chest",
-                "price": "200",
-                "armor": "10",
-                "attack": "0",
-                "health": "20",
-                "img": "http://test.datalb.ru/assets/Chest-1.png"
-            },
-            {
-                "id": "1",
-                "name": "green chest",
-                "price": "400",
-                "armor": "15",
-                "attack": "0",
-                "health": "30",
-                "img": "http://test.datalb.ru/assets/Chest-2.png"
-            },
-            {
-                "id": "2",
-                "name": "purple chest",
-                "price": "800",
-                "armor": "20",
-                "attack": "0",
-                "health": "40",
-                "img": "http://test.datalb.ru/assets/Chest-3.png"
-            }],
-            "boots": 
-            [{
-                "id": "0",
-                "name": "gray boots",
-                "price": "100",
-                "armor": "2",
-                "attack": "0",
-                "health": "5",
-                "img": "http://test.datalb.ru/assets/Boots-1.png"
-            },
-            {
-                "id": "1",
-                "name": "green boots",
-                "price": "200",
-                "armor": "4",
-                "attack": "0",
-                "health": "10",
-                "img": "http://test.datalb.ru/assets/Boots-2.png"
-            },
-            {
-                "id": "2",
-                "name": "purple boots",
-                "price": "400",
-                "armor": "6",
-                "attack": "0",
-                "health": "15",
-                "img": "http://test.datalb.ru/assets/Boots-3.png"
-            }],
-            "swords": 
-            [{
-                "id": "0",
-                "name": "gray sword",
-                "price": "500",
-                "armor": "0",
-                "attack": "15",
-                "health": "0",
-                "img": "http://test.datalb.ru/assets/Sword-1.png"
-            },
-            {
-                "id": "1",
-                "name": "green sword",
-                "price": "1000",
-                "armor": "0",
-                "attack": "25",
-                "health": "0",
-                "img": "http://test.datalb.ru/assets/Sword-2.png"
-            },
-            {
-                "id": "2",
-                "name": "purple sword",
-                "price": "1500",
-                "armor": "0",
-                "attack": "35",
-                "health": "0",
-                "img": "http://test.datalb.ru/assets/Sword-3.png"
-            }]
-        },
+        person:{},
+        equipment:{},
         totalCost: 0,
         currentEquipment: {}, // equipmentName: optionId
         currentStats: {},
@@ -170,12 +21,24 @@ class Calculator extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            currentStats: {
-                "health": this.state.person.health,
-                "armor": this.state.person.armor,
-                "attack": this.state.person.attack
-            }
+        const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+        const url ="http://test.datalb.ru/test.json"
+        fetch(proxyUrl + url)
+        .then(response => response.json())
+        .then(data => {
+            this.setState((state) => ({
+                person: data.person,
+                equipment: data.equipment
+            })
+        )})
+        .then(() => {
+            this.setState((state) => ({
+                currentStats: {
+                    "health": this.state.person.health,
+                    "armor": this.state.person.armor,
+                    "attack": this.state.person.attack
+                }
+            }))
         })
     }
 
